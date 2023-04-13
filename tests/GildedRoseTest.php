399e0f19412becef289893647f8a0a1b3f6bf343
Motassem <motassem.kassab@kilo.health>
2023-04-13 15:22:25 +0300

    added unit tests for initial logic

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -10,11 +10,223 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    /**
+     * @dataProvider commonProvider
+     */
+    public function testUpdateQuality(
+        string $name,
+        int $sellIn,
+        int $quality,
+        int $expectedSellIn,
+        int $expectedQuality,
+        ?string $expectedName = null,
+    ): void
     {
-        $items = [new Item('foo', 0, 0)];
+        if (null === $expectedName) {
+            $expectedName = $name;
+        }
+        $items = [new Item($name, $sellIn, $quality)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame($expectedName, $items[0]->name);
+        $this->assertSame($expectedSellIn, $items[0]->sellIn);
+        $this->assertSame($expectedQuality, $items[0]->quality);
+    }
+
+    private function commonProvider(): array
+    {
+        return [
+            'foo zero' => [
+                'name' => 'foo',
+                'sellIn' => 0,
+                'quality' => 0,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 0,
+                'expectedName' => 'foo',
+            ],
+            'brie zero' => [
+                'name' => GildedRose::NAME_BRIE,
+                'sellIn' => 0,
+                'quality' => 0,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 2,
+            ],
+            'tafka zero' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 0,
+                'quality' => 0,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 0,
+            ],
+            'sulf zero' => [
+                'name' => GildedRose::NAME_SULFURAS,
+                'sellIn' => 0,
+                'quality' => 0,
+                'expectedSellIn' => 0,
+                'expectedQuality' => 0,
+            ],
+            'sulf q > 0' => [
+                'name' => GildedRose::NAME_SULFURAS,
+                'sellIn' => 0,
+                'quality' => 1,
+                'expectedSellIn' => 0,
+                'expectedQuality' => 1,
+            ],
+            'foo q > 0' => [
+                'name' => 'foo',
+                'sellIn' => 0,
+                'quality' => 1,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 0,
+            ],
+            'brie q > 50' => [
+                'name' => GildedRose::NAME_BRIE,
+                'sellIn' => 0,
+                'quality' => 51,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 51,
+            ],
+            'tafka q > 50' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 0,
+                'quality' => 51,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 0,
+            ],
+            'tafka q > 50 s < 11' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 10,
+                'quality' => 51,
+                'expectedSellIn' => 9,
+                'expectedQuality' => 51,
+            ],
+            'tafka q > 50 s > 11' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 12,
+                'quality' => 51,
+                'expectedSellIn' => 11,
+                'expectedQuality' => 51,
+            ],
+            'tafka q > 50 s < 6' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 5,
+                'quality' => 51,
+                'expectedSellIn' => 4,
+                'expectedQuality' => 51,
+            ],
+            'tafka q < 50' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 0,
+                'quality' => 49,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 0,
+            ],
+            'tafka q < 50 s < 11' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 10,
+                'quality' => 49,
+                'expectedSellIn' => 9,
+                'expectedQuality' => 50,
+            ],
+            'tafka q < 50 s > 11' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 12,
+                'quality' => 49,
+                'expectedSellIn' => 11,
+                'expectedQuality' => 50,
+            ],
+            'tafka q < 50 s < 6' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 5,
+                'quality' => 49,
+                'expectedSellIn' => 4,
+                'expectedQuality' => 50,
+            ],
+
+            'tafka q < 49' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 0,
+                'quality' => 48,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 0,
+            ],
+            'tafka q < 49 s < 11' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 10,
+                'quality' => 48,
+                'expectedSellIn' => 9,
+                'expectedQuality' => 50,
+            ],
+            'tafka q < 49 s > 11' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 12,
+                'quality' => 48,
+                'expectedSellIn' => 11,
+                'expectedQuality' => 49,
+            ],
+            'tafka q < 49 s < 6' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => 5,
+                'quality' => 48,
+                'expectedSellIn' => 4,
+                'expectedQuality' => 50,
+            ],
+            'foo q < 50' => [
+                'name' => 'foo',
+                'sellIn' => 0,
+                'quality' => 49,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 47,
+            ],
+            'brie s < 0 q < 50' => [
+                'name' => GildedRose::NAME_BRIE,
+                'sellIn' => -1,
+                'quality' => 0,
+                'expectedSellIn' => -2,
+                'expectedQuality' => 2,
+            ],
+            'brie s < 0 q > 50' => [
+                'name' => GildedRose::NAME_BRIE,
+                'sellIn' => -1,
+                'quality' => 51,
+                'expectedSellIn' => -2,
+                'expectedQuality' => 51,
+            ],
+            'tafka s < 0' => [
+                'name' => GildedRose::NAME_TAFKA,
+                'sellIn' => -1,
+                'quality' => 0,
+                'expectedSellIn' => -2,
+                'expectedQuality' => 0,
+            ],
+            'sulf s < 0 q < 0' => [
+                'name' => GildedRose::NAME_SULFURAS,
+                'sellIn' => -1,
+                'quality' => -1,
+                'expectedSellIn' => -1,
+                'expectedQuality' => -1,
+            ],
+            'sulf s < 0 q > 0' => [
+                'name' => GildedRose::NAME_SULFURAS,
+                'sellIn' => -1,
+                'quality' => 1,
+                'expectedSellIn' => -1,
+                'expectedQuality' => 1,
+            ],
+            'foo s < 0 q > 0' => [
+                'name' => 'foo',
+                'sellIn' => -1,
+                'quality' => 1,
+                'expectedSellIn' => -2,
+                'expectedQuality' => 0,
+            ],
+            'foo s < 0 q < 0' => [
+                'name' => 'foo',
+                'sellIn' => -1,
+                'quality' => -1,
+                'expectedSellIn' => -2,
+                'expectedQuality' => -1,
+            ],
+        ];
     }
 }
